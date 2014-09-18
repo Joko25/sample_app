2a9e4cab969168ce234539507741289cfa4d7168
Joko Mulyono <jokomulyono696@gmail.com>
2014-09-18 11:44:21 +0700

    Finish layout and router

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,14 +6,14 @@ describe "Static pages" do
     before { visit root_path}
 
     it {should have_content('MYNesia')}
-    it {should have_title("Ruby on Rails Tutorial Sample App")}
+    it {should have_title("MYNesia")}
     it {should_not have_title('| Home')}
 
   end
 
   describe "Help page" do
     before {visit help_path}
-    it { should have_title("Ruby on Rails Tutorial Sample App | Help")}
+    it { should have_title("MYNesia | Help")}
     it { should have_content('Help')}
   end
 
@@ -21,14 +21,14 @@ describe "Static pages" do
     before {visit about_path}
 
     it {should have_content('About us')}
-    it {should have_title("Ruby on Rails Tutorial Sample App | About")}
+    it {should have_title("MYNesia | About")}
 
   end
 
   describe "Contact page" do
     before {visit contact_path}
 
-    it {should have_title("Ruby on Rails Tutorial Sample App | Contact")}
+    it {should have_title("MYNesia | Contact")}
     it {should have_content('Contact')}
     
   end
