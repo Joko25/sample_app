a7fd7940d454d75e65cf2414bb4a93b244f8bf66
Joko Mulyono <jokomulyono696@gmail.com>
2014-09-21 16:35:05 +0700

    Finish sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,8 +21,11 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
-  end  
-
+  end
+    
+  def edit
+    @user = User.find(params[:id])
+  end
   private
 
     def user_params
